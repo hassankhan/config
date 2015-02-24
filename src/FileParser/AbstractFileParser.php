d19953e73af99e68a6558ad09cc551b2e59eb29b
Hassan Khan <contact@hassankhan.me>
2015-02-24 22:44:47 +0000

    Added `AbstractFileParser`

---
+++ src/FileParser/AbstractFileParser.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Noodlehaus\FileParser;
+
+/**
+ * Abstract file parser
+ *
+ * @package    Config
+ * @author     Jesus A. Domingo <jesus.domingo@gmail.com>
+ * @author     Hassan Khan <contact@hassankhan.me>
+ * @link       https://github.com/noodlehaus/config
+ * @license    MIT
+ */
+abstract class AbstractFileParser implements FileParserInterface
+{
+
+    /**
+     * Path to the config file
+     *
+     * @var string
+     */
+    protected $path;
+
+    public function __construct($path)
+    {
+        $this->path = $path;
+    }
+}
